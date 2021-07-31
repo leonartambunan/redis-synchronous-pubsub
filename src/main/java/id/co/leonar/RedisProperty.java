a5bb6934d47873af2b0ab10c8d9b49ec5b7a5315
Leonar Tambunan <leonar.tambunan@gmail.com>
2021-07-31 19:13:59 +0700

    Create RedisProperty.java

---
+++ src/main/java/id/co/leonar/RedisProperty.java
@@ -0,0 +1,9 @@
+package id.co.leonar;
+
+public class RedisProperty {
+    public static String hostname="localhost";
+    public static int port= 6379;
+    public static String password="";
+    public static String requestChannelName="CHANNEL_REQUEST";
+    public static String responseChannelName="CHANNEL_RESPONSE";
+}
